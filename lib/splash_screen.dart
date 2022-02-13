70d650715852a2e396e468aee3c94bafa6bebca3
Tawhidul Arefin <tawhidularefin530@gmail.com>
2022-02-13 22:25:10 +0600

    Added: splash video

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:crypto_start_screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({ Key? key }) : super(key: key);
@@ -12,12 +14,27 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+  bool _visible = false;
 
   @override
   void initState() {
-    Timer(const Duration(minutes: 1), ()=>const WelcomeScreen());
+  
+
+    _controller = VideoPlayerController.asset("assets/vid.mp4");
+    _controller.initialize().then((_) {
+      _controller.setLooping(true);
+      Timer(const Duration(milliseconds: 100), () {
+        setState(() {
+          _controller.play();
+          _visible = true;
+        });
+        
+      });
     super.initState();
   }
+    );}
+
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +42,9 @@ class _SplashScreenState extends State<SplashScreen> {
       backgroundColor: Colors.black,
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Lottie.asset('assets/lottie.json'),
+            Container(color: Colors.transparent),
           ],
         ),
       ),
